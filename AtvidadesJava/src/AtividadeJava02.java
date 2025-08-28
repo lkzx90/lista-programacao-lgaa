e3f24f434fb058dc377311693dd3feb5eb14b711
lucasgabriel <tnluckas@gmail.com>
2025-08-28 19:44:50 -0300

    lk

---
+++ AtvidadesJava/src/AtividadeJava02.java
@@ -0,0 +1,11 @@
+public class AtividadeJava02 {
+    public static void main(String[] args) {
+        int dia = 6 ;
+        int mes = 7 ;
+        int ano = 2008 ;
+        System.out.println("nasci no dia "+dia);
+        System.out.println("mes " +mes);
+        System.out.println("do ano " +ano);
+
+    }
+}

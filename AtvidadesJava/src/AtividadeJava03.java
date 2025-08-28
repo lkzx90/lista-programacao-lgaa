1a65aac0e289e5630356bcfaf6af8eedab4a8af6
lucasgabriel <tnluckas@gmail.com>
2025-08-28 20:40:01 -0300

    lk

---
+++ AtvidadesJava/src/AtividadeJava03.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class AtividadeJava03 {
+    public static void main(String[] args) {
+
+    int qtdAlunos = 17;
+    double media = 10;
+    boolean aprovado = true , falso = false;
+    String professor = "Victor";
+
+    System.out.println("numeros de alunos "+qtdAlunos+ " media da turma " +media+ " aprovado ou reprovado " +aprovado+ " professor " +professor);}
+
+}

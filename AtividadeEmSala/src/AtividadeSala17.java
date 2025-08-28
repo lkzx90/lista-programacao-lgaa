98bd86096452f7d41ff7ebc397d06beaebeefd18
lucasgabriel <tnluckas@gmail.com>
2025-08-28 19:26:40 -0300

    lk

---
+++ AtividadeEmSala/src/AtividadeSala17.java
@@ -0,0 +1,2 @@
+public class AtividadeSala17 {
+}
---
+++ AtvidadesJava/src/AtividadeJava1.java
@@ -0,0 +1,9 @@
+public class AtividadeJava1 {
+    public static void main(String[] args) {
+
+        String nome = "Lucas";
+        String cidade = "Bh";
+        String pais = "Brasil";
+
+        System.out.println("Olá eu sou " +nome+ " moro em " +cidade + "/" + pais);}
+}

09180a825d3d2b14cd20d23f9dd4939f236337b1
lucasgabriel <tnluckas@gmail.com>
2025-08-28 22:00:40 -0300

    lk

---
+++ AtvidadesJava/src/AtividadeJava09.java
@@ -0,0 +1,10 @@
+public class AtividadeJava09 {
+    public static void main(String[] args) {
+        double base = 5;
+        double altura = 2;
+        double perimetro  = 2 * (base + altura);
+
+
+        System.out.println("o perimetro é " +perimetro);
+    }
+}
---
+++ AtvidadesJava/src/AtividadeJava10.java
@@ -0,0 +1,9 @@
+public class AtividadeJava10 {
+    public static void main(String[] args) {
+        double celsius = 25;
+        double fahrenheit = (9 * celsius) / 5 + 32;
+
+
+        System.out.println("O valor é: " +fahrenheit);
+    }
+}
